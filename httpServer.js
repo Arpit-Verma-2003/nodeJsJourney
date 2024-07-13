231833e8c0481ee001c516a0f12e0de86e747a8b
Arpit-Verma-2003 <arpitverma200316@gmail.com>
2024-07-14 00:27:22 +0530

    routes feature added

--- httpServer.js
+++ httpServer.js
@@ -1,8 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 const myServer = http.createServer((req,res)=>{
-    console.log("new req rec");
-    console.log(req.headers);
-    res.end("Hello from the Server");
+    // console.log(req.headers);
+    const log = `${Date.now()},${req.url}:new request recieved\n`;
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(req.url){
+            case '/': res.end("You are at Home Page");
+            break;
+            case '/about': res.end("Well I am a Student");
+            break;
+            case '/contact': res.end("Contact - abc@gmail.com");
+            break;
+            default: res.end("Oops Not Found");
+        }
+    })
 });
 
 myServer.listen(8085,()=>console.log("server is started here"));
